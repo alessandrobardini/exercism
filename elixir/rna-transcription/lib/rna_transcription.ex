d27b8ff3af3638388e873f0e5bf27972b03d3c02
alessandrobardini <alessandro.bardini93@gmail.com>
2019-12-26 23:17:31 +0100

    Elixir: RNA Transcription

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,21 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @dna_to_rna_map %{
+    ?G => ?C,
+    ?C => ?G,
+    ?T => ?A,
+    ?A => ?U
+  }
+
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.map(dna, &Map.get(@dna_to_rna_map, &1))
+  end
+end
