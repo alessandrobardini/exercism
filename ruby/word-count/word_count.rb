9e4b203a8bb7ff1cd1369640090beef0701eaaf7
alessandrobardini <alessandro.bardini93@gmail.com>
2019-12-26 23:19:14 +0100

    Ruby: word count

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,14 @@
+class Phrase
+  def initialize(phrase)
+    @phrase = phrase
+  end
+
+  def word_count
+    words.each_with_object(Hash.new(0)) { |word, counts| counts[word] += 1 }
+  end
+
+  private def words
+    @phrase.scan(/\w+(?:'\w+)*/)
+           .map(&:downcase)
+  end
+end
