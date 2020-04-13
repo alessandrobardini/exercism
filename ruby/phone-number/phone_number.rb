9922658f41ab7b83cdc0be2fc218129154258f7c
alessandrobardini <alessandro.bardini93@gmail.com>
2020-04-13 23:44:45 +0200

    Ruby: phone number"

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,14 +1,40 @@
 class PhoneNumber
-  def self.clean(phone_number)
-    cleaned_number = phone_number.scan(/\d+/)
-    
-    return nil if cleaned_number.size < 10 ||cleaned_number.size > 11
-    if cleaned_number.size == 11
-      if cleaned_number[0] != '1'
-        return nil
-      end
-      return cleaned_number[1..-1]
-    end
-    cleaned_number
+  def self.clean(number)
+    new(number).valid?
+  end
+
+  private
+
+  def initialize(number)
+    number = number.delete('^0-9')
+    @number = has_country_code?(number) ? number_without_country_code(number) : number
+  end
+
+  def has_country_code?(number)
+    number.size == 11 && valid_country?(number)
+  end
+
+  def valid_country?(number)
+    number[0] == '1'
+  end
+
+  def number_without_country_code(number)
+    number[1..-1]
+  end
+
+  attr_reader :number
+
+  def valid_exchange_code?
+    number[0] > '1'
+  end
+
+  def valid_subscriber_number?
+    number[3] > '1'
+  end
+
+  public
+
+  def valid?
+    number.size == 10 && valid_exchange_code? && valid_subscriber_number? ? number: nil
   end
 end
